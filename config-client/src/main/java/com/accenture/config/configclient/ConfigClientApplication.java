ec96950ef8f72abf8fa15c8f2ca13e4e60b8aab7
Z0139 <Z0139@montepio.com>
2021-10-22 23:54:06 +0100

    V4

--- config-client/src/main/java/com/accenture/config/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/accenture/config/configclient/ConfigClientApplication.java
@@ -2,10 +2,25 @@ package com.accenture.config.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConfigClientApplication {
 
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to java techie!";
+	}
+	
+	@GetMapping("/{input}")
+	public String congrats(@PathVariable String input) {
+		return "hi" + input + " bla bla";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
