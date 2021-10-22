8e6ee98b5997d1caefbe4c78c8d7d80d7ed6c669
Z0139 <Z0139@montepio.com>
2021-10-22 23:23:18 +0100

    V3

--- config-client/src/main/java/com/accenture/config/configclient/MessageResource.java
+++ config-client/src/main/java/com/accenture/config/configclient/MessageResource.java
@@ -6,12 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
-@RequestMapping("/")
 @RestController
 public class MessageResource {
 
-	@Value("${message: Default Hello}")
+	@Value("${message: Default Hello!!!}")
 	private String message;
 	
 	@GetMapping("/")
